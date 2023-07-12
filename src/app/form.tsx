ce1ae9bacbb4b490deea3c6f161a344e1166867d
Ahmed Hany <dev.ahmedhany@gmail.com>
2023-07-12 14:12:15 +0300

    complete form

--- src/app/form.tsx
+++ src/app/form.tsx
@@ -10,14 +10,17 @@ export default function Form() {
         const form = e.target as HTMLFormElement
         const input = form.elements[0] as HTMLInputElement
         if(!input.value) return
-        const sensor_id = input.value
-        form.reset()
+        const wristband_id = input.value
+        const place = (form.elements[1] as HTMLSelectElement).value
+        const isIn = (form.elements[2] as HTMLInputElement).checked
+        input.value = ''
         //send to firestore
         const db = getFirestore();
         try {
           const docRef = await addDoc(collection(db, "checkinandout"), {
-            sensor_id,
-            place: "bus",
+            wristband_id,
+            place,
+            isIn,
             timestamp: serverTimestamp()
           });
           console.log("Document written with ID: ", docRef.id);
@@ -26,6 +29,20 @@ export default function Form() {
         }
       }}>
       <input className="rfid-input" name='rfid' autoFocus type="text" placeholder="RFID" />
+      <br />
+      <br />
+      <br />
+      <br />
+      <select className="place-input" name="place">
+        <option value="class">Class</option>
+        <option value="bus">Bus</option>
+      </select>
+      <br />
+      <br />
+      <br />
+      <br />
+      <input type='checkbox' name='isIn' value='true' defaultChecked />
+      <label htmlFor='isIn'>In</label>
       </form>
   )
 }
