2ad6de6235d675f53788e031a4672e3cf7cb452c
Ahmed Hany <dev.ahmedhany@gmail.com>
2023-07-12 13:57:37 +0300

    create docs

---
+++ src/app/firebase.tsx
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyABhqFlm36WsOh1k4mdWvJGylawnI9_SNE",
+  authDomain: "gradproject-1e033.firebaseapp.com",
+  projectId: "gradproject-1e033",
+  storageBucket: "gradproject-1e033.appspot.com",
+  messagingSenderId: "718255456414",
+  appId: "1:718255456414:web:1f772a8b4218ce7613fb8f"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
--- src/app/form.tsx
+++ src/app/form.tsx
@@ -1,17 +1,29 @@
 'use client'
+import './firebase'
+import { getFirestore, collection, addDoc, serverTimestamp } from "firebase/firestore";
 
 export default function Form() {
   return (
 
-      <form onSubmit={(e)=>{
+      <form onSubmit={async (e)=>{
         e.preventDefault()
         const form = e.target as HTMLFormElement
         const input = form.elements[0] as HTMLInputElement
         if(!input.value) return
-        console.log(input.value)
-        //TODO: send to server
-        
+        const sensor_id = input.value
         form.reset()
+        //send to firestore
+        const db = getFirestore();
+        try {
+          const docRef = await addDoc(collection(db, "checkinandout"), {
+            sensor_id,
+            place: "bus",
+            timestamp: serverTimestamp()
+          });
+          console.log("Document written with ID: ", docRef.id);
+        } catch (e) {
+          console.error("Error adding document: ", e);
+        }
       }}>
       <input className="rfid-input" name='rfid' autoFocus type="text" placeholder="RFID" />
       </form>
